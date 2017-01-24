4770b595f9d85ce5526645ae1ee2694fa7f067c5
mjuferdi <mariomuhammad@Marios-MBP-2.fritz.box>
2017-01-24 16:43:41 +0100

    Added Book Model, Book admin class, change store template

--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Book
 
 
 def index(request):
     return render(request, 'template.html')
 
+
 def store(request):
-    return render(request, 'store.html')
+    # GETTING DATA FROM DATABASE
+    count = Book.objects.all().count()
+    context = {
+        'count' : count,
+    }
+    return render(request, 'store.html', context)
